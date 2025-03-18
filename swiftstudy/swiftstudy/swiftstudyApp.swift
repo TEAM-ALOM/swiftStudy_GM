4cf5f637fa19816fce3106ba71d6eb63bb9f8596
moo1677 <lgm04@naver.com>
2025-03-18 18:18:16 +0900

    singleton 구현

---
+++ swiftstudy/swiftstudy/swiftstudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftstudyApp.swift
+//  swiftstudy
+//
+//  Created by 이건무 on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftstudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
