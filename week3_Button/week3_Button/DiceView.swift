8be6549b95d938dfb275ccf2a3ae745b4cc82eb5
moo1677 <lgm04@naver.com>
2025-03-23 01:26:24 +0900

    week3

--- week3_Button/week3_Button/DiceView.swift
+++ week3_Button/week3_Button/DiceView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DiceView: View {
     @State private var numberOfPips : Int = 1   //주사위 점 개수
-    // rgb값을 렌덤으로
+    // rgb값을 랜덤으로
     @State private var r : Double = .random(in: 0...1)
     @State private var g : Double = .random(in: 0...1)
     @State private var b : Double = .random(in: 0...1)
@@ -19,14 +19,14 @@ struct DiceView: View {
             Image(systemName :"die.face.\(numberOfPips)")  //numberofPips만큼 주사위의 점 개수 설정
                 .resizable()    //아이콘 크기 편집 가능하게 만듦
                 .frame(maxWidth :100, maxHeight : 100)  //아이콘 크기 설정, max로 설정해 최대값을 100으로 설정(다이스 개수가 많아지면 크기 조절 가능)
-                .aspectRatio(1, contentMode: .fit)
-                .foregroundColor(Color(red: r, green: g, blue: b))
+                .aspectRatio(1, contentMode: .fit) // aspectRatio는 비율 조절, contentMode:.fit은 공간에 맞게 크기 조절
+                .foregroundColor(Color(red: r, green: g, blue: b)) //아이콘 색상 랜덤하게 변경
             Button("Roll"){
                 withAnimation{
-                    numberOfPips = .random(in: 1...6)
+                    numberOfPips = .random(in: 1...6) // 버튼을 누르면 numberOfPips가 1~6까지의 랜덤한 숫자로 변경
                 }
             }
-            .buttonStyle(.bordered)
+            .buttonStyle(.bordered) //버튼 스타일에 테두리 적용
         }
     }
     
--- week3_Button/week3_Button/UnDiceView.swift
+++ week3_Button/week3_Button/UnDiceView.swift
@@ -8,24 +8,26 @@
 import SwiftUI
 
 struct UnDiceView: View {
-    @Binding var numberOfDice:Int
+    @Binding var numberOfDice:Int //contentview에 numberOfDice를 바인딩으로 불러와 값을 변경하면 contentview에서도 변경되게 만듦
     var body: some View {
         VStack {
+            //물음표 아이콘 설정
             Image(systemName :"questionmark")
-                .resizable()
-                .frame(width :60, height : 100)
-                .aspectRatio(1, contentMode: .fit)
+                .resizable()  //크기 변경 가능
+                .frame(width :60, height : 100) //사이즈 적용
+                //.aspectRatio(1, contentMode: .fit)
                 .padding()
+                //다이스를 추가하는 버튼
             Button("Add a Dice"){
                 withAnimation{
-                    numberOfDice += 1
+                    numberOfDice += 1 //버튼 클릭시 numberofdice의 값이 1 추가됨
                 }
             }
-            .buttonStyle(.bordered)
+            .buttonStyle(.bordered) //버튼 스타일 테두리로 변경
         }
     }
 }
 
 #Preview {
-    UnDiceView(numberOfDice: .constant(1))
+    UnDiceView(numberOfDice: .constant(1)) //numberofdice를 1로 설정해 미리 확인 할 수 있음
 }
